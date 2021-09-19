4b64454c785f9ed8f49c919b0e68533f3477d100
darakuneko <tvkids@gmail.com>
2021-09-19 09:17:33 +0900

    update: gpk60-33r

--- qmk/gpk60_33r/keymaps/default/keymap.c
+++ qmk/gpk60_33r/keymaps/default/keymap.c
@@ -18,15 +18,15 @@
 // Defines names for use in layer keycodes and the keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_Q,                  KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT, \
-    MT(MOD_LCTL,KC_TAB),   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-    MO(1),                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  MT(MOD_RSFT,KC_SLSH)                                                                
+    KC_Q,                  KC_W,          KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,           KC_ENT, \
+    MT(MOD_LCTL,KC_TAB),   KC_A,          KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,           KC_SCLN, \
+    MT(MOD_LSFT,KC_SPC),   LT(1, KC_Z),   KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, LT(1, KC_DOT),  MT(MOD_RSFT,KC_BSPC)                                                                
   ),
 
   [1] = LAYOUT(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC, \
-    KC_LSFT, KC_GRV,  KC_BSLS, KC_MINS, KC_EQL,  KC_QUOT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LGUI, \
-    KC_TRNS, KC_HENK, KC_SLSH, KC_LBRC, KC_RBRC,  KC_SPC, KC_BSPC,  KC_TRNS, KC_TRNS, KC_MHEN, MO(2)
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    MT(MOD_LSFT,KC_ESC), \
+    KC_LSFT, KC_GRV,  KC_BSLS, KC_MINS, KC_EQL,  KC_QUOT, KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, KC_LGUI, \
+    KC_HENK, KC_TRNS, KC_SLSH, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_MHEN, MO(2)
   ),
 
   [2] = LAYOUT(
