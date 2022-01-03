f40b3f0ea2662fcf19b8212c50e40b05a183e96d
darakuneko <tvkids@gmail.com>
2022-01-03 10:40:26 +0900

    fix: numnum ver1.2

--- qmk/num_num_strawberry/keymaps/v1_1/keymap.c
+++
@@ -1,6 +0,0 @@
-#include "../default.c"
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { clockwise ? encoder_cw(is_hold) : encoder_ccw(is_hold); }
-    return true;
-}
--- qmk/num_num_strawberry/keymaps/v1_2/keymap.c
+++
@@ -1,6 +0,0 @@
-#include "../default.c"
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { clockwise ? encoder_ccw() : encoder_cw(); }
-    return true;
-}
