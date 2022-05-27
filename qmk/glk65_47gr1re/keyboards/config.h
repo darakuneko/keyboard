dfe76b252f9e31b40a67b999f12660ce1536e8b3
darakuneko <tvkids@gmail.com>
2022-05-27 15:37:31 +0900

    fix: debounce

--- qmk/glk65_47gr1re/keyboards/config.h
+++ qmk/glk65_47gr1re/keyboards/config.h
@@ -94,7 +94,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 #undef LOCKING_RESYNC_ENABLE
 
-#define DEBOUNCE 50
+#define DEBOUNCE 5
 
 /* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
  * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
