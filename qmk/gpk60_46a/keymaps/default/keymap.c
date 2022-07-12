92fe211b25b8e3d379de569e1acfccb40b472e7e
darakuneko <tvkids@gmail.com>
2022-07-12 16:33:11 +0900

    fix: 46A keymap

--- qmk/gpk60_46a/keymaps/default/keymap.c
+++ qmk/gpk60_46a/keymaps/default/keymap.c
@@ -35,19 +35,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,    _______,    _______,    _______,    _______,         _______,    _______,    _______,    _______,    _______,    _______, \
     _______,                          _______,    _______,    _______,         _______,    _______,    _______,                            _______ \
   ),
-  [0] = LAYOUT(
+  [3] = LAYOUT(
     KC_ESC,              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
     MT(MOD_LCTL,KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, \
     KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT, \
     KC_LALT,                             MO(5) ,   MO(4),   KC_SPC,        KC_ENT,  KC_BSPC, MO(4),                MO(5)         \
   ),
-  [1] = LAYOUT(
+  [4] = LAYOUT(
     S(KC_GRV), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),      S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_MINS), S(KC_EQL),
     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_MINS, KC_EQL, \
     _______,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,         _______, _______, _______, _______, _______, _______, \
     KC_LGUI,               MO(6),    _______,    _______,        _______, _______, MO(6),                      KC_LGUI \
   ),
-  [2] = LAYOUT(
+  [5] = LAYOUT(
     _______,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,           KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, KC_F12, \
     _______,  _______,    _______,    _______,    _______,    _______,         _______,    _______,    _______,    _______,    _______,    _______, _______, \
     _______,  _______,    _______,    _______,    _______,    _______,         _______,    _______,    _______,    _______,    _______,    _______, \
