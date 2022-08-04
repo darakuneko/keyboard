9ddbf9a996b694b37b2b8188491f61e8dc7ca799
darakuneko <tvkids@gmail.com>
2022-08-05 00:20:41 +0900

    fix: unnecessary settings

--- qmk/yamada_shozo/config.h
+++ qmk/yamada_shozo/config.h
@@ -53,8 +53,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define I2C1_SCL_PIN B8 
 #define I2C1_SDA_PIN B9
-#define I2C1_SCL_PAL_MODE 4
-#define I2C1_SDA_PAL_MODE 4
 #define I2C1_CLOCK_SPEED  400000
 #define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2
 #define OLED_DISPLAY_128X64
