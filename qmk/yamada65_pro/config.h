a39f6afe8c5618505166b9cc046e6710ca534105
darakuneko <tvkids@gmail.com>
2022-08-06 17:43:41 +0900

    add: yamada65 pro

---
+++ qmk/yamada65_pro/config.h
@@ -0,0 +1,32 @@
+/*
+Copyright 2022 daraku_neko
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x1523
+#define DEVICE_VER   0x0001
+#define MANUFACTURER Daraku-Neko
+#define PRODUCT      Yamada65 Pro
+
+#define DEBOUNCE 50
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
---
+++ qmk/yamada65_pro/rules.mk
@@ -0,0 +1,3 @@
+DEBOUNCE_TYPE = sym_eager_pk
+VIA_ENABLE = yes
+SRC += qmk_rc.c
