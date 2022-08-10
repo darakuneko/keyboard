c2060e7aba60947ea95057b3c7beaca7f1af1940
darakuneko <tvkids@gmail.com>
2022-08-10 17:14:50 +0900

    fix: yamada65 pro adjustment

--- qmk/yamada65_pro/config.h
+++ qmk/yamada65_pro/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MANUFACTURER Daraku-Neko
 #define PRODUCT      Yamada65 Pro
 
-#define MATRIX_IO_DELAY 300
+#define MATRIX_IO_DELAY 500
 #define DEBOUNCE 25
 #define LOCKING_SUPPORT_ENABLE
 #define LOCKING_RESYNC_ENABLE
