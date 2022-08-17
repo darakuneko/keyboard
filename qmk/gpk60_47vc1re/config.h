94f13678e97ab635cb1fbdb5ec5626820698a732
darakuneko <tvkids@gmail.com>
2022-08-17 17:11:25 +0900

    add: gpk60_47vc1re

--- qmk/gpk60_47vc1re/config.h
+++ qmk/gpk60_47vc1re/config.h
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xA3A3
+#define VENDOR_ID    0xFEED
 #define PRODUCT_ID   0x1427
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Daraku-Neko
