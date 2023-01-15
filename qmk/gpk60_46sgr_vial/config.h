bea70d3ceb07bb72f6dc28cf93ff9c07d70324f0
darakuneko <tvkids@gmail.com>
2023-01-15 21:31:16 +0900

    add: 46w/46sgr for vial

---
+++ qmk/gpk60_46sgr_vial/config.h
@@ -0,0 +1,25 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define VIAL_KEYBOARD_UID {0x3D, 0xDE, 0x03, 0x26, 0x96, 0x67, 0xCE, 0xD2}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0}
+#define VIAL_UNLOCK_COMBO_COLS {0}
---
+++ qmk/gpk60_46sgr_vial/keymaps/default/keymap.c
@@ -0,0 +1,71 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[0] = LAYOUT(
+		
+		KC_ESC, KC_Q, KC_P, KC_RBRC, KC_LBRC, 
+		MT(MOD_LCTL,KC_TAB), KC_A, KC_SCLN, KC_QUOT, KC_BSLS, 
+		KC_LSFT, KC_Z, KC_SLSH, MT(MOD_LSFT, KC_SCLN), 
+		KC_LALT, KC_RALT, 
+		KC_W, KC_E, KC_R, KC_T, 
+		KC_S, KC_D, KC_F, KC_G, 
+		KC_X, KC_C, KC_V, KC_B, 
+		KC_LGUI, MO(2), LT(1, KC_SPC), 
+		KC_Y, KC_U, KC_I, KC_O, 
+		KC_H, KC_J, KC_K, KC_L, 
+		KC_N, KC_M, KC_COMM, KC_DOT, 
+		LT(1, KC_ENT), LT(2, KC_BSPC), KC_RGUI
+	),
+
+	[1] = LAYOUT(
+		
+		KC_GRV, KC_NO, KC_NO, KC_MINS, KC_EQL, 
+		KC_NO, KC_1, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_6, KC_NO, KC_NO, 
+		KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_2, KC_3, KC_4, KC_5, 
+		KC_7, KC_8, KC_9, KC_NO, 
+		KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO
+	),
+
+	[2] = LAYOUT(
+		
+		KC_NO, KC_F1, KC_F10, KC_F11, KC_F12, 
+		RGB_TOG, RGB_VAI, KC_NO, KC_NO, KC_NO, 
+		KC_NO, RGB_VAD, KC_NO, KC_NO, 
+		EE_CLR, QK_BOOT, 
+		KC_F2, KC_F3, KC_F4, KC_F5, 
+		RGB_SAI, RGB_HUI, RGB_SPI, KC_NO, 
+		RGB_SAD, RGB_HUD, RGB_SPD, KC_NO, 
+		KC_NO, KC_NO, KC_NO, 
+		KC_F6, KC_F7, KC_F8, KC_F9, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO
+	),
+
+	[3] = LAYOUT(
+		
+		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO, KC_NO, 
+		KC_NO, KC_NO, KC_NO
+	),
+
+};
