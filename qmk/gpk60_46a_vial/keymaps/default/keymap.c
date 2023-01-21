e5914a24c28e55160244c3a2b7c90e7eb017d4dd
darakuneko <tvkids@gmail.com>
2023-01-21 23:53:11 +0900

    add: gpk60-46a vial

--- qmk/gpk60_46a_vial/keymaps/default/keymap.c
+++ qmk/gpk60_46a_vial/keymaps/default/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		MT(MOD_LCTL,KC_TAB),KC_A,    KC_SCLN, KC_QUOT, KC_BSLS,
 		KC_LSFT, KC_Z,    KC_COMM, KC_DOT,  MT(MOD_LSFT, KC_SCLN),
 		KC_LALT, KC_RALT,
-		KC_W,    KC_R,    KC_T,    KC_Y,
+		KC_W,    KC_E,    KC_R,    KC_T,
 		KC_S,    KC_D,    KC_F,    KC_G,
 		KC_X,    KC_C,    KC_V,    KC_B,
 		KC_LGUI, MO(2),   LT(1, KC_SPC),
