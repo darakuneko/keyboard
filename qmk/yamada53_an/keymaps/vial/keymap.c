dea4818310f406858aad6928b3bb49daee8ff8ce
darakuneko <tvkids@gmail.com>
2023-04-29 12:06:50 +0900

    fix: keymap

--- qmk/yamada53_an/keymaps/vial/keymap.c
+++ qmk/yamada53_an/keymaps/vial/keymap.c
@@ -5,8 +5,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT(
-		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, CTL_T(KC_Y),
-		MT(MOD_LCTL,KC_TAB),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  CTL_T(KC_Z),
+		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+		MT(MOD_LCTL,KC_TAB),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 		KC_UP,
 		KC_LGUI, KC_LALT, MO(2),   LT(1, KC_SPC),LT(1, KC_BSPC),MO(2),   KC_RALT, KC_RGUI,
