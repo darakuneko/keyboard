28bf8d4b3b2823a6f393985e77a798951fde3aae
darakuneko <tvkids@gmail.com>
2023-06-11 16:16:52 +0900

    add: trident40

---
+++ qmk/trident40/config.h
@@ -0,0 +1,28 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEC, 0xD0, 0x64, 0x23, 0x9A, 0x23, 0xD8, 0xFB}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
