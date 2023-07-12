086ca85549122a3476f69dd3cdfa54870ca8f76f
darakuneko <tvkids@gmail.com>
2023-07-12 16:41:39 +0900

    add: grin type-r vial

---
+++ qmk/grin_type_r/config.h
@@ -0,0 +1,25 @@
+// Copyright 2022 GRINkeebs (@policium)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDB, 0x64, 0x72, 0x8A, 0x12, 0x3F, 0x40, 0xF6}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define I2C1_SDA_PIN        GP26
+#define I2C1_SCL_PIN        GP27
+
+/* Haptic feedback */
+#define FB_ERM_LRA 0
+#define FB_BRAKEFACTOR 3 // For 1x:0, 2x:1, 3x:2, 4x:3, 6x:4, 8x:5, 16x:6, Disable Braking:7
+#define FB_LOOPGAIN 1 // For  Low:0, Medium:1, High:2, Very High:3
+/* Motor settings */
+#define RATED_VOLTAGE 3
+#define V_PEAK 5
+#define HAPTIC_FEEDBACK_DEFAULT 0
+#define DRV_MODE_DEFAULT 0
+#define DRV_GREETING       alert_750ms
