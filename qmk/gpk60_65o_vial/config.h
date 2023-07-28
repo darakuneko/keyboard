189719d005ec43152af67ea7a9eb257fea690298
darakuneko <tvkids@gmail.com>
2023-07-28 11:29:36 +0900

    add: gpk60-65o v2

---
+++ qmk/gpk60_65o_vial/config.h
@@ -0,0 +1,28 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4D, 0xE8, 0xA8, 0x67, 0xDE, 0x41, 0xBA, 0x3C}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
