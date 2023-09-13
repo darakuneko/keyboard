4254ec4080a010f9e93652952ef3cc51ad3782cd
Daraku-Neko <5214078+darakuneko@users.noreply.github.com>
2023-09-13 11:30:26 +0900

    add: cornelius vial

---
+++ qmk/cornelius/config.h
@@ -0,0 +1,21 @@
+/*
+Copyright 2020 foostan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xEC, 0xE1, 0x7D, 0x1A, 0xE0, 0xF2, 0xF7, 0x3A}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
