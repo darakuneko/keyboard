4be91debc8959841b5455f2316070a38be641853
Daraku-Neko <5214078+darakuneko@users.noreply.github.com>
2023-11-25 22:37:27 +0900

    update: promicro 46w vial

--- qmk/gpk60_46w/config.h
+++ qmk/gpk60_46w/config.h
@@ -1,79 +1,25 @@
-/*
-Copyright 2021 daraku_neko
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x1418
-#define DEVICE_VER   0x0001
-#define MANUFACTURER Daraku-Neko
-#define PRODUCT      GPK60-46W
-
-/* key matrix size */
-#define MATRIX_ROWS 7
-#define MATRIX_COLS 7
-
 /*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
  */
-#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3, B2 }
-#define MATRIX_COL_PINS { D0, D4, C6, D7, E6, B4, B5 }
 
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+/* disable debug print */
+//#define NO_DEBUG
 
-#define RGB_DI_PIN D3
-#ifdef RGB_DI_PIN
-  #define RGBLED_NUM 11
-  #define RGBLIGHT_HUE_STEP 16
-  #define RGBLIGHT_SAT_STEP 16
-  #define RGBLIGHT_VAL_STEP 16
-  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-  #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+/* disable print */
+//#define NO_PRINT
 
-  /*== or choose animations ==*/
-  #define RGBLIGHT_EFFECT_BREATHING
-  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-  #define RGBLIGHT_EFFECT_SNAKE
-  #define RGBLIGHT_EFFECT_KNIGHT
-  #define RGBLIGHT_EFFECT_CHRISTMAS
-  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-  #define RGBLIGHT_EFFECT_RGB_TEST
-  #define RGBLIGHT_EFFECT_ALTERNATING
-// /*== customize breathing effect ==*/
-//   /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
-//   #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
-//   /*==== use exp() and sin() ====*/
-//   #define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-//   #define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-#endif
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
 
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
+#define VIAL_KEYBOARD_UID {0xCC, 0x42, 0xB1, 0x59, 0xE3, 0xBA, 0xD5, 0xA4}
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
