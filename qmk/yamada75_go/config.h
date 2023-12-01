554886e56fa7f2778676c248abc848c29315de1f
Daraku-Neko <tvkids@gmail.com>
2023-12-01 22:02:14 +0900

    update: yamada75 go vial

---
+++ qmk/yamada75_go/config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE2, 0xEB, 0x49, 0x91, 0x50, 0x93, 0x62, 0x82}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
