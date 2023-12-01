6509901b96d986b442ba72de1d70daad3881b3fc
Daraku-Neko <tvkids@gmail.com>
2023-12-01 21:23:05 +0900

    update: 60r 65o vial

---
+++ qmk/gpk60_60r/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD6, 0xF2, 0xB8, 0x6A, 0xDE, 0xAB, 0x25, 0x9E}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
---
+++ qmk/gpk60_65o/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x90, 0x7E, 0x12, 0x22, 0x78, 0x61, 0xC2, 0x11}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
