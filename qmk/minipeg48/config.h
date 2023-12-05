885899cda32047b641f5ca12adee45dcb96c45ae
darakuneko <tvkids@gmail.com>
2023-12-05 14:29:23 +0900

    add: minipeg48

---
+++ qmk/minipeg48/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 sporewoh (@sporewoh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE1, 0x36, 0xE1, 0x41, 0xA6, 0xE1, 0x22, 0x3A}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
