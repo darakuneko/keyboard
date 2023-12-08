15aa25817431a12f525f74e19290e80f5f0d7eb6
darakuneko <tvkids@gmail.com>
2023-12-08 16:50:27 +0900

    add: cornelius_mod

---
+++ qmk/cornelius_mod/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x12, 0xD9, 0x32, 0x3B, 0x65, 0x0A, 0x3E, 0xB9}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
