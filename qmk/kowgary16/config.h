e3dc6368368e38112f6d700dcdd863a64c4fec2e
Daraku-Neko <5214078+darakuneko@users.noreply.github.com>
2023-12-09 12:40:50 +0900

    add: kowgary16

---
+++ qmk/kowgary16/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 sporewoh (@sporewoh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7A, 0xE7, 0x39, 0x38, 0x2E, 0x9A, 0x88, 0x45}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
