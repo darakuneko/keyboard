4b497e3c20674a967914620a43bcb348a3450d54
darakuneko <tvkids@gmail.com>
2023-12-18 22:56:03 +0900

    update: 63r layer color

--- qmk/gpk60_63r/keymaps/vial/keymap.c
+++ qmk/gpk60_63r/keymaps/vial/keymap.c
@@ -2,8 +2,33 @@
 
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+typedef struct _hsv_layer_t {
+    uint8_t hue : 8;
+    uint8_t sat : 8;
+    uint8_t val : 8;
+	bool change : 1;
+} hsv_layer_t;
 
+typedef struct _hsv_layers_t {
+    hsv_layer_t hsv_layer0;
+	hsv_layer_t hsv_layer1;
+	hsv_layer_t hsv_layer2;
+	hsv_layer_t hsv_layer3;
+} hsv_layers_t;
+
+hsv_layers_t hsv_layers;
+
+bool is_setting_hsv_layer;
+
+enum {
+  START_COLOR_LAYER = QK_KB_0,
+  SET_COLOR_LAYER0,
+  SET_COLOR_LAYER1,
+  SET_COLOR_LAYER2,
+  SET_COLOR_LAYER3
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -23,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[2] = LAYOUT(
 		XXXXXXX, RGB_VAI, RGB_SAI, RGB_HUI, RGB_SPI, RGB_MOD, RGB_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR,
 		XXXXXXX, RGB_VAD, RGB_SAD, RGB_HUD, RGB_SPD, RGB_RMOD,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
+		START_COLOR_LAYER, SET_COLOR_LAYER0, SET_COLOR_LAYER1, SET_COLOR_LAYER2, SET_COLOR_LAYER3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 	),
@@ -35,21 +60,103 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 	),
-
 };
+
+void keyboard_post_init_user(void) {
+	eeconfig_read_user_datablock(&hsv_layers);
+	is_setting_hsv_layer = false;
+}
+
+void update_hsv_layer(keyrecord_t *record, int layer_num) {
+	if (record->event.pressed) {
+		if(is_setting_hsv_layer){
+			hsv_layer_t hsv;
+			hsv.hue = rgblight_get_hue();
+			hsv.sat = rgblight_get_sat();
+			hsv.val = rgblight_get_val();
+			hsv.change = true;
+			if(layer_num == 1){
+				hsv_layers.hsv_layer1 = hsv;
+			} else if(layer_num == 2){
+				hsv_layers.hsv_layer2 = hsv;
+			} else if(layer_num == 3){
+				hsv_layers.hsv_layer3 = hsv;
+			} else {
+				hsv_layers.hsv_layer0 = hsv;
+			}
+			uprintf("change %d\n", hsv_layers.hsv_layer0.change);
+			eeconfig_update_user_datablock(&hsv_layers);
+			is_setting_hsv_layer = false;
+		}
+	}
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+	case START_COLOR_LAYER:
+		if (record->event.pressed) {
+			is_setting_hsv_layer = !is_setting_hsv_layer;
+		}
+        return false;
+	case SET_COLOR_LAYER0:
+		update_hsv_layer(record, 0);
+        return false;
+	case SET_COLOR_LAYER1:
+		update_hsv_layer(record, 1);
+        return false;
+	case SET_COLOR_LAYER2:
+		update_hsv_layer(record, 2);
+        return false;
+	case SET_COLOR_LAYER3:
+		update_hsv_layer(record, 3);
+        return false;
+ 	default:
+    	return true;
+  }
+}
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) { 
   int current_layer = get_highest_layer(layer_state|default_layer_state);  
   HSV hsv = {0, 255, rgblight_get_val()};
-  if (current_layer == 1) {
-    hsv.h = 191; //PURPLE
-  } else if (current_layer == 2)  {
-    hsv.h = 85; //GREEN
-  } else if (current_layer == 3)  {
-    hsv.h = 43; //YELLOW
-  } else {
-    hsv.h = 128; //CYAN
+  
+  if(is_setting_hsv_layer){
+  	hsv = rgblight_get_hsv();
+  } else{
+	if (current_layer == 1) {
+		if(!hsv_layers.hsv_layer1.change) {
+			hsv.h = 191; //PURPLE
+		} else {
+			hsv.h = hsv_layers.hsv_layer1.hue;
+			hsv.s = hsv_layers.hsv_layer1.sat;
+			hsv.v = hsv_layers.hsv_layer1.val;
+		}
+  	} else if (current_layer == 2)  {
+		if(!hsv_layers.hsv_layer2.change) {
+    		hsv.h = 85; //GREEN
+		} else {
+			hsv.h = hsv_layers.hsv_layer2.hue;
+			hsv.s = hsv_layers.hsv_layer2.sat;
+			hsv.v = hsv_layers.hsv_layer2.val;
+		}
+  	} else if (current_layer == 3)  {
+ 		if(!hsv_layers.hsv_layer3.change) {
+    		hsv.h = 43; //YELLOW
+		} else {
+			hsv.h = hsv_layers.hsv_layer3.hue;
+			hsv.s = hsv_layers.hsv_layer3.sat;
+			hsv.v = hsv_layers.hsv_layer3.val;
+		}
+  	} else {
+ 		if(!hsv_layers.hsv_layer0.change) {
+			hsv.h = 128; //CYAN
+		} else {
+			hsv.h = hsv_layers.hsv_layer0.hue;
+			hsv.s = hsv_layers.hsv_layer0.sat;
+			hsv.v = hsv_layers.hsv_layer0.val;
+		}
+  	}
   }
+
   RGB rgb = hsv_to_rgb(hsv);
   
   for (uint8_t i = led_min; i <= led_max -1; i++) {
