723a9f449cf2d7ad6d38c825ba54f4ca0ee1d04a
darakuneko <tvkids@gmail.com>
2024-02-08 16:06:36 +0900

    fix: gpk60-46a keymap

--- qmk/gpk60_46a/keymaps/vial/keymap.c
+++ qmk/gpk60_46a/keymaps/vial/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
     MT(MOD_LCTL,KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, \
     KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT, \
-    KC_LGUI,                             MO(2) ,   KC_LALT,   LT(1, KC_SPC),LT(1, KC_BSPC),  KC_BSPC, KC_RALT,               MO(2)       \
+    KC_LGUI,                             MO(2) ,   KC_LALT,   LT(1, KC_SPC),LT(1, KC_ENT),  KC_BSPC, KC_RALT,               MO(2)       \
   ),
      
 	[1] = LAYOUT(
