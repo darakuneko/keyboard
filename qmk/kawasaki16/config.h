76b7083242d48fc72da4ddc197f21b92c953d528
darakuneko <tvkids@gmail.com>
2024-04-11 16:17:57 +0900

    add: kawasaki16

---
+++ qmk/kawasaki16/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x85, 0xD3, 0x2A, 0x4B, 0xB5, 0x18, 0xBD, 0xA8}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
