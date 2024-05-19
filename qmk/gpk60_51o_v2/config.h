a9f9a01fac5f2112f8c7cdb416b5dec68b859478
darakuneko <tvkids@gmail.com>
2024-05-19 17:24:52 +0900

    add: gpk60_51o v2

---
+++ qmk/gpk60_51o_v2/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x24, 0x6D, 0xAD, 0x84, 0x76, 0xA6, 0xE6, 0x1F}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
