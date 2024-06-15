d9a461eb6fc0e951d5e121e26470e755b0b5da0d
darakuneko <tvkids@gmail.com>
2024-06-15 13:05:55 +0900

    add: gpk60l-46w v2

---
+++ qmk/gpk60l_46w_v2/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA2, 0x21, 0x75, 0x99, 0xF2, 0xBD, 0xD9, 0x4B}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
