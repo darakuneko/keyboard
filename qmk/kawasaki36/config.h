d7464ee93257e9dab917a4d2c5ecbc34bc27eab0
Daraku-Neko <tvkids@gmail.com>
2024-07-11 22:12:01 +0900

    add: kawasaki36

---
+++ qmk/kawasaki36/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4C, 0xAB, 0x32, 0x4D, 0xD3, 0x07, 0x4F, 0xD4}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {1, 0}
