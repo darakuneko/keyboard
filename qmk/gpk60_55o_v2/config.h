557285d30841b5fab6c70c50a172f31f9702764e
Daraku-Neko <tvkids@gmail.com>
2025-04-01 22:10:50 +0900

    add: gpk60-55o v2

---
+++ qmk/gpk60_55o_v2/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE8, 0x0E, 0x52, 0x78, 0x34, 0xAD, 0xB5, 0x94}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
