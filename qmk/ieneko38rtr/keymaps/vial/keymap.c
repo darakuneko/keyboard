8c409e7fcf3ebab7b86da14f5ef8b55ee5f7f02d
Daraku-Neko <tvkids@gmail.com>
2025-06-04 18:32:29 +0900

    fix: keymap.c

--- qmk/ieneko38rtr/keymaps/vial/keymap.c
+++ qmk/ieneko38rtr/keymaps/vial/keymap.c
@@ -54,7 +54,7 @@ int lasted_layer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
   [0] = LAYOUT(
-	  KC_ESC,              QK_BOOT,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+	  KC_ESC,              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 		MT(MOD_LCTL,KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
 		KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
 		           KC_MNXT,           KC_RALT, LT(1, KC_SPC),LT(1, KC_BSPC),MO(2),  
