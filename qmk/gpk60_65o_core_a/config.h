5b165f472d893631f582d23600a298aa24ce5f33
darakuneko <tvkids@gmail.com>
2025-06-11 00:14:39 +0900

    Update gpk60_65o_core_a_vial.uf2, gpk60_65o_core_a.zip, config.h, and 5 more files

---
+++ qmk/gpk60_65o_core_a/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4D, 0xE8, 0xA8, 0x67, 0xDE, 0x41, 0xBA, 0x3C}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
