9ef203e7965ff338592afadc2f032a4243b62a08
darakuneko <tvkids@gmail.com>
2025-06-17 13:39:46 +0900

    Update trident40_core_a_vial.uf2, config.h, info.json, and 13 more files

---
+++ qmk/trident40_core_a/config.h
@@ -0,0 +1,9 @@
+// Copyright 2025 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEC, 0xD0, 0x64, 0x23, 0x9A, 0x23, 0xD8, 0xFB}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
