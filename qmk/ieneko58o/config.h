72db0c002c0f076a1ae7e5277e8e88806e9ad1dc
Daraku-Neko <tvkids@gmail.com>
2025-08-23 14:15:22 +0900

    add: ieneko58o

---
+++ qmk/ieneko58o/config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define WS2812_PWM_DRIVER PWMD1
+#define WS2812_PWM_CHANNEL 2
+#define WS2812_PWM_PAL_MODE 1
+#define WS2812_PWM_COMPLEMENTARY_OUTPUT
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM6
+#define WS2812_DMA_CHANNEL 7 
+
+#define VIAL_KEYBOARD_UID {0x18, 0xE8, 0x38, 0x07, 0x32, 0x0C, 0x4A, 0xF3}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
