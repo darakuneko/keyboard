4fa4a730795a7eb3f25af5cf5c293d4edde3e5fb
darakuneko <tvkids@gmail.com>
2026-08-27 14:51:26 +0900

    Update gpk60_46sgr_v3_vial.bin, gpk60_46sgr_v3.zip, config.h, and 7 more files

---
+++ qmk/gpk60_46sgr_v3/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define WS2812_PWM_DRIVER PWMD3
+#define WS2812_PWM_CHANNEL 3
+#define WS2812_PWM_PAL_MODE 1
+#define WS2812_PWM_DMA_STREAM STM32_DMA1_STREAM3
+#define WS2812_PWM_DMA_CHANNEL 3
+
+#define VIAL_KEYBOARD_UID {0x6A, 0xC9, 0xBB, 0x7F, 0x6F, 0xA6, 0xE2, 0xC5}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
